23bf41511fc415fa60279af2bc0a1f7461baaf3d
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-03-02 23:09:27 -0300

    day04

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -32,19 +32,20 @@ scissors = '''
 #Write your code below this line 👇
 game_images = [rock, paper, scissors]
 computer_choice = random.randint(0,2)
-print(game_images[choice])
-print("Computer choice:")
-print(game_images[computer_choice])
 
-if choice == 0 and computer_choice == 2:
-    print("You win!")
-elif computer_choice == 0 and choice == 2:
-    print("You lose")
-elif choice >=3 or choice < 0 :
+if choice >=3 or choice < 0 :
     print("You typed an invalid number, you lose!")
-elif computer_choice > choice :
-    print("You lose")
-elif computer_choice < choice :
-    print("You win!")
-elif choice == computer_choice:
-    print("thats a draw")
+else:
+    print(game_images[choice])
+    print("Computer choice:")
+    print(game_images[computer_choice])
+    if choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and choice == 2:
+        print("You lose")
+    elif computer_choice > choice :
+        print("You lose")
+    elif computer_choice < choice :
+        print("You win!")
+    elif choice == computer_choice:
+        print("thats a draw")
