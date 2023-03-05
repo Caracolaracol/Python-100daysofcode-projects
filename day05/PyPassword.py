4ab55834a1f64640cba18b1bd619be3814adedab
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-03-05 00:32:56 -0300

    day06

--- day05/PyPassword.py
+++ day05/PyPassword.py
@@ -8,7 +8,7 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-2
+
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 # lengthLetters = len(letters) - 1
---
+++ day06/functions.py
@@ -0,0 +1,14 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+
+
+#defining function
+
+def my_function():
+    print("Hello")
+    print("Bye")
+    
+
+my_function()
