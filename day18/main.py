10da9b3229953389e00b5d804a43eedfbabe1d2f
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-05-02 20:36:17 -0400

    day18

---
+++ day18/main.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.forward(200)
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick() # this stays at the bottom
